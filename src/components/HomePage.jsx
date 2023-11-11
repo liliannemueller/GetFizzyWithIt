1493452593915268ccfd64fe6718eab29d7d0c4c
Lilianne Mueller <lilyanneom@gmail.com>
2023-11-10 17:02:11 -0800

    fizzy meter added, correctly logging data, needs connection with db

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,18 +2,43 @@ import React, { useState } from "react";
 import { Link, Route, Switch } from "react-router-dom";
 import PlacesAutoComplete from './PlacesAutoComplete'
 import Bar from "./Bar";
+import FizzyMeter from './FizzyMeter'
 
 function HomePage( {onSelectBar, barData} ) {
   const [selectedBar, setSelectedBar] = useState(null);
+  const [fizzyValue, setFizzyValue] = useState(0);
 
   const handleSelectBar = (barData) => {
     setSelectedBar(barData);
+  };
+  const handleFizzyMeterChange = (event) => {
+    setFizzyValue(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(fizzyValue)
+    //add logic to add fizzy rating to database
+    const data = {
+        name: selectedBar.value,  
+        place_id: selectedBar.placeId,
+        fizzyRating: fizzyValue,
+    }
+    console.log("BarDATA:",data)
+    setFizzyValue(0);
   };
 
   return (
     <div>
       <PlacesAutoComplete onSelectBar={handleSelectBar} />
-      {selectedBar && <Bar barData={selectedBar} />}
+      {selectedBar && (
+      <div>
+        <Bar barData={selectedBar} />
+        <form onSubmit={handleSubmit}>
+                <FizzyMeter value={fizzyValue} onChange={handleFizzyMeterChange} />
+                <button type="submit">Submit Rating</button>
+        </form>
+      </div>
+    )}
     </div>
   );
 }
