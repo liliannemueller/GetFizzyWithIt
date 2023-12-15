2a9e6df398908df80fef7083788f95578b2eef98
Lilianne Mueller <lilyanneom@gmail.com>
2023-12-15 14:50:41 -0800

    bar review styling, range, h4

--- src/components/FizzyMeter.jsx
+++ src/components/FizzyMeter.jsx
@@ -11,9 +11,9 @@ export default function FizzyMeter({ value, onChange }){
             <label htmlFor="fizzyMeter" className="form-label">How fizzy is it?</label>
         </div>
         <div className= "meter-container">
-            <h4>flat</h4>
+            <h4 id="flat">flat</h4>
             <input type="range" className="form-range" min="0" max="4" step="1" id="fizzyMeter" value={value} onChange={onChange}/> 
-            <h4>fizzy</h4>
+            <h4 id="fizzy">fizzy</h4>
         </div>
         
         </div>
